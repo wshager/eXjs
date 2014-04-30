0f8a5c452b7ed995feecaf52431bc53c5f3d0c24
whager <whager@benjamins.nl>
2014-04-30 19:31:01 +0200

    initial commit

---
+++ src/main/java/org/exist/js/xquery/Module.java
@@ -0,0 +1,66 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2012-2013 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.exist.js.xquery;
+
+import java.util.List;
+import java.util.Map;
+
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+
+public class Module extends AbstractInternalModule {
+	
+	public final static String NAMESPACE_URI = "http://exist-db.org/js";
+	public final static String PREFIX = "js";
+	
+	private final static FunctionDef[] functions = {
+		new FunctionDef(RunScript.signature, RunScript.class),
+	};
+
+	public Module(Map<String, List<? extends Object>> parameters) {
+		super(functions, parameters);
+	}
+
+	@Override
+	public String getDefaultPrefix() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNamespaceURI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getReleaseVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/main/java/org/exist/js/xquery/RunScript.java
@@ -0,0 +1,51 @@
+package org.exist.js.xquery;
+
+import org.exist.dom.QName;
+import org.exist.xquery.*;
+import org.exist.xquery.value.*;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class RunScript extends BasicFunction {
+	public final static FunctionSignature signature = 
+	new FunctionSignature(
+		new QName("run", Module.NAMESPACE_URI, Module.PREFIX),
+		"A useless example function. It just echoes the input parameters.",
+		new SequenceType[] { 
+			new FunctionParameterSequenceType("text", Type.STRING, Cardinality.ZERO_OR_MORE, "The script to run")
+		},
+		new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO_OR_MORE, "The script result"));
+	public RunScript(XQueryContext context) {
+		super(context, signature);
+	}
+	public Sequence eval(Sequence[] args, Sequence contextSequence)
+		throws XPathException {
+		// is argument the empty sequence?
+		
+	 	// Creates and enters a Context. The Context stores information
+		// about the execution environment of a script.
+		@SuppressWarnings("deprecation")
+		Context cx = Context.enter();
+		try {
+			// Initialize the standard objects (Object, Function, etc.)
+			// This must be done before scripts can be executed. Returns
+			// a scope object that we use in later calls.
+			Scriptable scope = cx.initStandardObjects();
+			
+			// Collect the arguments into a single string.
+			String s = "";
+			for (SequenceIterator i = args[0].iterate(); i.hasNext();) {
+				s += i.nextItem().getStringValue();
+			}
+			
+			// Now evaluate the string we've collected.
+			Object result = cx.evaluateString(scope, s, "<cmd>", 1, null);
+			// Convert the result to a string and print it.
+			return new StringValue(Context.toString(result));
+			
+		} finally {
+			// Exit from the context.
+			Context.exit();
+		}
+	}
+}
